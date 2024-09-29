caf20435b2ea0ba42f6750958b191643b09b3398
Filipsys <filipsysak@gmail.com>
2024-09-29 19:37:56 +0200

    Mostly finished the main look, added the necessary text and images. Overall look finished

--- app/page.tsx
+++ app/page.tsx
@@ -4,20 +4,20 @@ function App() {
   return (
     <div className="w-screen bg-gray-200 text-zinc-800 flex flex-col">
       <div className="flex flex-row">
-        <div className="w-1/2 p-12">
+        <div className="w-1/2 p-12 pr-8">
           <div>
             <h2 className="font-bold text-2xl">Krosser</h2>
           </div>
 
           <div className="h-full flex items-center">
             <h1 className="text-7xl pt-12">
-              The world's leading anti-fraud platform for financial institutions
+              The World's Leading Anti-Fraud Platform for Financial Institutions
             </h1>
           </div>
         </div>
 
         <div className="w-1/2 p-12 flex flex-col">
-          <nav className="flex flex-row gap-16">
+          <nav className="flex flex-row gap-16 underline-offset-4">
             <ul>
               <li>
                 <a className="hover:underline" href="#">
@@ -43,29 +43,29 @@ function App() {
               </li>
               <li>
                 <a className="hover:underline" href="#">
-                  Contact us
+                  Contact Us
                 </a>
               </li>
               <li>
                 <a className="hover:underline" href="#">
-                  Our blog
+                  Our Blog
                 </a>
               </li>
             </ul>
             <ul>
               <li>
                 <a className="hover:underline" href="#">
-                  International Partners
+                  Case Studies
                 </a>
               </li>
               <li>
                 <a className="hover:underline" href="#">
-                  Contact us
+                  Resources & Insights
                 </a>
               </li>
               <li>
                 <a className="hover:underline" href="#">
-                  Our blog
+                  Innovation Hub
                 </a>
               </li>
             </ul>
@@ -82,7 +82,7 @@ function App() {
         </div>
       </div>
 
-      <div className=" flex mt-12 items-center overflow-hidden">
+      <div className=" flex mt-12 items-center p-4">
         <div className="w-[150vw] flex flex-row gap-4">
           <div className="relative">
             <Image
@@ -90,11 +90,11 @@ function App() {
               alt="First image"
               width={700}
               height={700}
-              className="aspect-[9/16] object-cover h-auto"
+              className="aspect-[9/16] object-cover brightness-50"
             />
-            <p className="absolute top-0 right-0 p-4 text-right w-2/3">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum,
-              veniam?
+            <p className="absolute top-0 right-0 p-4 text-right w-3/4 text-white">
+              No need to worry about fraud, take your business performance to
+              the next stage.
             </p>
           </div>
           <div className="relative">
@@ -103,12 +103,12 @@ function App() {
               alt="Second image"
               width={700}
               height={700}
-              className="aspect-[9/16] object-cover h-auto"
+              className="aspect-[9/16] object-cover brightness-50"
             />
 
-            <p className="absolute top-0 right-0 p-4 text-right w-2/3 text-white">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum,
-              veniam?
+            <p className="absolute top-0 right-0 p-4 text-right w-3/4 text-white">
+              Finance in ease, no more hassle. Krosser is the best choice for
+              your organization.
             </p>
           </div>
           <div className="relative">
@@ -118,12 +118,11 @@ function App() {
               width={700}
               height={700}
               priority
-              className="aspect-[9/16] object-cover h-auto"
+              className="aspect-[9/16] object-cover brightness-50"
             />
 
-            <p className="absolute top-0 right-0 p-4 text-right w-2/3 text-white">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum,
-              veniam?
+            <p className="absolute top-0 right-0 p-4 text-right w-3/4 text-white">
+              Less time stressing spent on meetings, more time on your business.
             </p>
           </div>
         </div>
@@ -132,7 +131,7 @@ function App() {
       <div className="flex flex-col p-12">
         <div>
           <h2 className="text-7xl pb-12 w-2/3">
-            One subsciption, one platform, no hassle
+            Single Subscription, One Comprehensive Platform, Zero Hassle
           </h2>
         </div>
 
@@ -140,7 +139,7 @@ function App() {
           <div className="grid grid-cols-2 grid-rows-2 w-1/3 p-4 ">
             <div />
             <div />
-            <div className="flex items-end text-l">
+            <div className="flex items-end text-l ">
               See our platform in action and discover how it elevates your fraud
               prevention effortlessly.
             </div>
@@ -162,19 +161,136 @@ function App() {
       </div>
 
       <div className="flex flex-col p-12 bg-gray-300">
-        <div className="grid grid-cols-5 grid-rows-3 w-full">
-          <h2>Trusted by thousands</h2>
+        <div className="grid grid-cols-5 grid-rows-2 w-full *:p-2">
+          <div>
+            <h2>Trusted by thousands, including our esteemed gold partners.</h2>
+          </div>
           <div />
-          <div />
-          <div />
-          <div />
+          <div>
+            <Image
+              className="opacity-30"
+              src="/logos/3m.svg"
+              alt="3M Logo"
+              width={64}
+              height={64}
+            />
+          </div>
+          <div>
+            <Image
+              className="opacity-30"
+              src="/logos/airbnb.svg"
+              alt="Airbnb Logo"
+              width={64}
+              height={64}
+            />
+          </div>
+          <div>
+            <Image
+              className="opacity-30"
+              src="/logos/amazon.svg"
+              alt="Amazon Logo"
+              width={64}
+              height={64}
+            />
+          </div>
 
           <div />
           <div />
-          <div />
-          <div />
-          <div />
+          <div>
+            <Image
+              className="opacity-30"
+              src="/logos/lidl.svg"
+              alt="Lidl Logo"
+              width={64}
+              height={64}
+            />
+          </div>
+          <div>
+            <Image
+              className="opacity-30"
+              src="/logos/alfaromeo.svg"
+              alt="Microsoft Logo"
+              width={64}
+              height={64}
+            />
+          </div>
+          <div>
+            <Image
+              className="opacity-30"
+              src="/logos/theboringcompany.svg"
+              alt="TheBoringCompany Logo"
+              width={64}
+              height={64}
+            />
+          </div>
         </div>
+      </div>
+
+      <div className="w-screen bg-gray-200 flex flex-col gap-12 p-12">
+        <div className="w-full flex flex-row gap-12">
+          <div className="w-full h-96">
+            <h3 className="text-6xl py-6">
+              Round-the-Clock Support for Uninterrupted Security
+            </h3>
+            <p className="text-xl">
+              Our dedicated support team ensures your institution's protection
+              is always in capable hands. With 24/7 monitoring and real-time
+              assistance, we respond to your concerns any time, minimizing risk
+              and maximizing operational security.
+            </p>
+          </div>
+          <div className="w-full h-96">
+            <Image
+              src="/images/other-img-1.jpg"
+              alt="Server image"
+              width={1000}
+              height={1000}
+              style={{ width: "100%", height: "100%" }}
+            />
+          </div>
+        </div>
+
+        <div className="w-full flex flex-row gap-12">
+          <div className="w-full h-96">
+            <Image
+              src="/images/other-img-2.jpg"
+              alt="Processor image"
+              width={1000}
+              height={1000}
+              style={{ width: "100%", height: "100%" }}
+            />
+          </div>
+          <div className="w-full h-96">
+            <h3 className="text-6xl py-6">
+              Advanced Fraud Detection with AI-Driven Precision
+            </h3>
+            <p className="text-xl">
+              Harness the power of artificial intelligence to safeguard your
+              financial ecosystem. Our cutting-edge platform identifies
+              suspicious activity in real time, mitigating threats with
+              unparalleled accuracy and efficiency, all while adapting to
+              evolving fraud techniques.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-screen bg-gradient-to-t from-gray-300 to-gray-200 flex flex-row justify-between p-12 text-gray-400">
+        <h3 className="text-xl">Krosser</h3>
+
+        <div className="flex flex-row gap-4">
+          <p className="hover:underline underline-offset-4">
+            <a href="#">Terms and conditions</a>
+          </p>
+          <p className="hover:underline underline-offset-4">
+            <a href="#">Privacy policy</a>
+          </p>
+          <p className="hover:underline underline-offset-4">
+            <a href="#">Cookie policy</a>
+          </p>
+        </div>
+
+        <p>All rights reserved ©2024</p>
       </div>
     </div>
   );
